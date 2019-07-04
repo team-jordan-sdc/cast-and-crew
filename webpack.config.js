3103801d61cce2a9ddb6edc4287b4d0a8fb71422
Will Resen <willresen@gmail.com>
2019-07-04 14:39:24 -0700

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
+  }
+};
