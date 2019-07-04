2c8a61f552005c223f0321f2bbd2b6fbe9db9c20
Will Resen <willresen@gmail.com>
2019-07-04 15:44:20 -0700

    Begin database seeding script

---
+++ database/seed.js
@@ -0,0 +1,30 @@
+
+/*************
+ * PERSONNEL *
+ *************/
+
+const actors = [
+  "Elvis Presley",
+  "John Travolta",
+  "John Belushi",
+  "George Clooney",
+  "Natalie Portman",
+  "Scarlett Johannsson",
+  "Marlon Brando",
+  "Margot Robbie",
+  "Will Smith",
+  "Chris Evans",
+  "Robert Downey Jr"
+];
+
+const headshots = [];
+const movie_ids = [];
+
+
+
+/**********
+ * MOVIES *
+ **********/
+
+
+ const movies = [];
