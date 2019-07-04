c1df6e311cc8131e7b0ddc60292862026965ccbc
Will Resen <willresen@gmail.com>
2019-07-04 15:07:06 -0700

    Change entrypoint from js to jsx

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.js`,
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
     path: DIST_DIR,
     filename: 'bundle.js'
