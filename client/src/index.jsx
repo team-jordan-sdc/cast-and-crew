55a1ecaf2412fd1bfecfd620e43fe2a15df4d531
Will Resen <willresen@gmail.com>
2019-07-05 22:31:16 -0700

    Begin implementing components + API requests

---
+++ client/src/index.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const Movie = require('./components/Movie.jsx');
+const Person = require('./components/Person.jsx');
+const MoviesCarousel = require('./components/MoviesCarousel.jsx');
+const PersonnelCarousel = require('./components/PersonnelCarousel.jsx');
