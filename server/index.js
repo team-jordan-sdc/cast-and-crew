a5f8e16e5cc767c47ec5031d329c2c7eca77f47e
Will Resen <willresen@gmail.com>
2019-07-05 23:00:56 -0700

    Changed "req.body" to "req.query" for GET requests

--- server/index.js
+++ server/index.js
@@ -8,13 +8,13 @@ app.use(express.static('./client/dist'));
 app.use(express.json());
 
 app.get('/api/personnel', (req, res) => {
-  db.getPersonnel(req.body.id)
+  db.getPersonnel(req.query.id)
     .then(results => res.send(results))
     .catch(err => console.log(err))
 });
 
 app.get('/api/movies', (req, res) => {
-  db.getMovies(req.body.id)
+  db.getMovies(req.query.id)
     .then(results => results.filter(movie => movie.personnel.some(person => person.id === req.body.id)))
     .then(results => res.send(results))
     .catch(err => console.log(err))
