848e32ef33f16546165a3e3182e04bdf370bf420
Will Resen <willresen@gmail.com>
2019-07-05 17:11:14 -0700

    Implement query response to GET request @ "/api/personnel"

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const Promise = require('bluebird');
+const db = require('../database/index.js');
 const app = express();
 const PORT = 3000;
 
 app.use(express.static('./client/dist'));
 app.use(express.json());
+
+app.get('/api/personnel', (req, res) => {
+  db.getPersonnel(req.body.id)
+    .then(results => res.send(results))
+    .catch(err => console.log(err))
+})
 
 app.listen(PORT, () => `Listening on port ${3000}!`);
