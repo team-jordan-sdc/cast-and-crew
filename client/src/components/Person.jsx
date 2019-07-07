6fbe0cf050e8d80f0dc88fdd514a555087b1d19b
Will Resen <willresen@gmail.com>
2019-07-06 19:56:38 -0700

    Render personnel headshot, name and role

--- client/src/components/Person.jsx
+++ client/src/components/Person.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Person = ({info}) => (
   <div className="container">
-    <div className="headshot"><img src={info.thumbnail_url}></img></div>
-    <div className="name">{info.name}</div>
-    <div className="role">{info.role[0]}</div>
+    <div className="headshot"><img src={info._id.thumbnail_url}></img></div>
+    <div className="name">{info._id.name}</div>
+    <div className="role">{info.role}</div>
   </div>
 )
 
--- client/src/components/PersonnelCarousel.jsx
+++ client/src/components/PersonnelCarousel.jsx
@@ -9,30 +9,12 @@ class PersonnelCarousel extends React.Component {
     }
   }
 
-  componentDidMount(){
-    var personnel = {};
-
-    this.props.movie.personnel.forEach(person => {
-      if(!personnel[person.id]) {
-        personnel[person.id] = {role: [person.role]}
-      } else {
-        personnel[person.id].role.push(person.role);
-      }
-    })
-
-    this.props.personnel.forEach(person => {
-      personnel[person._id].thumbnail_url = person.thumbnail_url;
-      personnel[person._id].name = person.name;
-    })
-
-    this.setState({personnel: personnel});
-  }
 
   render(){
     return(
       <div className="container">
-        {Object.keys(this.state.personnel).map(person => {
-          return <div className="person"><Person info={this.state.personnel[person]}/></div>
+        {this.props.personnel.map(person => {
+          return <div className="person"><Person info={person}/></div>
         })}
       </div>
     )
