2289ff914d48a769c19418245a1bdeeca11bd888
Will Resen <willresen@gmail.com>
2019-07-07 11:19:10 -0700

    Implement getPersonnelInfo to retrieve movie list for selected cast-crew member

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PersonnelCarousel from './components/PersonnelCarousel.jsx';
+import MoviesCarousel from './components/MoviesCarousel.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -8,14 +9,15 @@ class App extends React.Component {
     this.state = {
       personnel: [],
       featuredMovie: {},
-    }
+      featuredPersonnel: null
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getFeaturedMovie('feature=true');
   }
 
-  getFeaturedMovie(querystring){
+  getFeaturedMovie(querystring) {
     return fetch(`/api/movies?${querystring}`, {
       method: 'GET',
       headers: { 'Content-Type': 'application/json' },
@@ -25,10 +27,20 @@ class App extends React.Component {
       .then(() => this.setState({personnel: this.state.featuredMovie.personnel}));
   }
 
+  getPersonnelInfo(id) {
+    fetch(`/api/personnel?id=${id}`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(results => results.json())
+    .then(results => this.setState({featuredPersonnel: results}))
+  }
+
   render() {
     return this.state.personnel.length ? (
       <div className="container">
-        <PersonnelCarousel personnel={this.state.personnel} movie={this.state.featuredMovie} />
+        <PersonnelCarousel personnel={this.state.personnel} movie={this.state.featuredMovie} set={this.getPersonnelInfo.bind(this)}/>
+        <MoviesCarousel featuredPersonnel={this.state.featuredPersonnel} />
       </div>
     ) : <div>Loading...</div>
   }
