4a96c57324c44f96c2ad4d6455824da6463bec0d
Will Resen <willresen@gmail.com>
2019-07-06 22:29:25 -0700

    Implement logic that determines when to stop carousel

--- client/src/components/PersonnelCarousel.jsx
+++ client/src/components/PersonnelCarousel.jsx
@@ -5,17 +5,31 @@ class PersonnelCarousel extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      position: 0,
       personnel: {},
     }
+    this.moveForward = this.moveForward.bind(this);
+    this.moveBackward = this.moveBackward.bind(this);
   }
 
+  moveBackward(){
+    this.state.position !== 0 && this.setState({position: this.state.position + 800});
+  };
 
-  render(){
-    return(
-      <div className="container">
-        {this.props.personnel.map(person => {
-          return <div className="person"><Person info={person}/></div>
-        })}
+  moveForward(){
+    this.state.position > Object.keys(this.props.personnel).length * -100 && this.setState({position: this.state.position - 800});
+  }
+
+  render() {
+    return (
+      <div id="carousel_container">
+        <div id="carousel" style={{transform: `translate3d(${this.state.position}px, 0px, 0px)`}}>
+          {this.props.personnel.map(person => {
+            return <div className="person"><Person info={person} /></div>
+          })}
+        </div>
+        <button onClick={() => this.moveBackward()}>Backward</button>
+        <button onClick={this.moveForward}>Forward</button>
       </div>
     )
   }
