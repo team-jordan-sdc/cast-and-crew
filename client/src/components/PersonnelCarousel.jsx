1605b23b7ef26f6c3687b663bcaa6ca941afa405
Will Resen <willresen@gmail.com>
2019-07-07 13:10:21 -0700

    Refactor moveBackward + Forward logic to restrict nav to window length

--- client/src/components/PersonnelCarousel.jsx
+++ client/src/components/PersonnelCarousel.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Person from './Person.jsx';
+import $ from 'jquery';
 
 class PersonnelCarousel extends React.Component {
   constructor(props){
@@ -13,23 +14,31 @@ class PersonnelCarousel extends React.Component {
   }
 
   moveBackward(){
-    this.state.position !== 0 && this.setState({position: this.state.position + 800});
+    if (this.state.position + 800 > 0) {
+      this.setState({position: 0});
+    } else {
+      this.setState({position: this.state.position + 800});
+    }
   };
 
   moveForward(){
-    this.state.position > Object.keys(this.props.personnel).length * -100 && this.setState({position: this.state.position - 800});
+    if(($('#personnel_carousel').position().left - 800) * -1 > $('#personnel_carousel').width() - $(window).width()) {
+      this.setState({position: ($('#personnel_carousel').width() - $(window).width()) * -1});
+    } else {
+      this.setState({position: this.state.position - 800});
+    }
   }
 
   render() {
     return (
-      <div id="carousel_container">
-        <div id="carousel" style={{transform: `translate3d(${this.state.position}px, 0px, 0px)`}}>
-          {this.props.personnel.map(person => {
-            return <div className="person"><Person info={person} /></div>
-          })}
-        </div>
-        <button onClick={() => this.moveBackward()}>Backward</button>
-        <button onClick={this.moveForward}>Forward</button>
+      <div className="carousel_container">
+          <div id="p_backward" onClick={this.moveBackward}></div>
+          <div id="personnel_carousel" style={{ transform: `translate3d(${this.state.position}px, 0px, 0px)` }}>
+            {this.props.personnel.map(person => {
+              return <div className="person"><Person info={person} set={this.props.set}/></div>
+            })}
+          </div>
+          <div id="p_forward" onClick={this.moveForward}></div>
       </div>
     )
   }
