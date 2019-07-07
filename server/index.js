0a4834e1ac2f606569d536c5416f99ea0a7d0187
Will Resen <willresen@gmail.com>
2019-07-06 17:04:55 -0700

    Modify getMovies to either send a random movie or all movies that feature a person

--- server/index.js
+++ server/index.js
@@ -14,9 +14,13 @@ app.get('/api/personnel', (req, res) => {
 });
 
 app.get('/api/movies', (req, res) => {
-  db.getMovies(req.query.id)
-    .then(results => results.filter(movie => movie.personnel.some(person => person.id === req.body.id)))
+  req.query.id && db.getMovies(req.query.id)
+    .then(results => results.filter(movie => movie.personnel.some(person => person.id === req.query.id)))
     .then(results => res.send(results))
+    .catch(err => console.log(err))
+
+  req.query.feature && db.getMovies()
+    .then(results => res.send(results[Math.floor(Math.random() * 99)]))
     .catch(err => console.log(err))
 });
 
