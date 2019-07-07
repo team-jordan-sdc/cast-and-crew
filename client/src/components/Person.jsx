e56a5b7e6f25811dc659ed2150a5e999f6ebf26c
Will Resen <willresen@gmail.com>
2019-07-06 17:53:56 -0700

    Add text for role

--- client/src/components/Person.jsx
+++ client/src/components/Person.jsx
@@ -4,6 +4,7 @@ const Person = ({info}) => (
   <div className="container">
     <div className="headshot"><img src={info.thumbnail_url}></img></div>
     <div className="name">{info.name}</div>
+    <div className="role">{info.role[0]}</div>
   </div>
 )
 
