868f37835c0f1c08f3076e5d53ad27b242c11a2e
Will Resen <willresen@gmail.com>
2019-07-08 14:41:21 -0700

    Config for NODE_ENV.test

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ console.log('Babel config detected!');
 
 module.exports = function (api) {
   const isTest = api.env('test');
-  const presets = ['@babel/preset-env', '@babel/preset-react'];
+  const presets = ['@babel/preset-env', '@babel/preset-react']
   const plugins = ['@babel/plugin-transform-runtime'];
   return {
     presets,
