722fa01644a3a7e4210b0fc8debe516f9a38759f
Will Resen <willresen@gmail.com>
2019-07-07 20:05:19 -0700

    Refactor moveBackward + Forward logic to restrict nav to window length

--- client/src/components/MoviesCarousel.jsx
+++ client/src/components/MoviesCarousel.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Movie from './Movie.jsx';
+import $ from 'jquery';
 
 class MovieCarousel extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      movies: this.props.featuredPersonnel
+      movies: this.props.featuredPersonnel,
+      position: 0
     }
+    this.moveForward = this.moveForward.bind(this);
+    this.moveBackward = this.moveBackward.bind(this);
   }
 
   componentDidUpdate(prevProps) {
@@ -16,15 +20,34 @@ class MovieCarousel extends React.Component {
     }
   }
 
+  moveBackward() {
+    if (this.state.position + 800 > 0) {
+      this.setState({position: 0});
+    } else {
+      this.setState({position: this.state.position + 800});
+    }
+  };
+
+  moveForward() {
+    if(($('#movies_carousel').position().left - 800) * -1 > $('#movies_carousel').width() - $(window).width()) {
+      this.setState({position: ($('#movies_carousel').width() - $(window).width()) * -1});
+    } else {
+      this.setState({position: this.state.position - 800});
+    }
+  }
+
   render() {
     return this.state.movies ? (
       <div className="carousel_container">
-        <div id="movies_carousel"></div>
-        {this.state.movies.map(movie => {
-          return (
-            <div className="movie"><Movie movie={movie}/></div>
-          )
-        })}
+        <div id="m_backward" onClick={this.moveBackward}></div>
+        <div id="movies_carousel" style={{ transform: `translate3d(${this.state.position}px, 0px, 0px)` }}>
+          {this.state.movies.map(movie => {
+            return (
+              <div className="movie"><Movie movie={movie} /></div>
+            )
+          })}
+        </div>
+        <div id="m_forward" onClick={this.moveForward}></div>
       </div>
     ) : <div></div>
   }
