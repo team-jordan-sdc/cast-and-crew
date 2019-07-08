80fb5c8c6b00c58e89e19c5ffc27f3d12838118c
Will Resen <willresen@gmail.com>
2019-07-08 14:45:01 -0700

    Update setupTests.js directory

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
-  setupFilesAfterEnv: ["<rootDir>/client/src/setupTests.js"],
+  setupFiles: ["<rootDir>/spec/setupTests.js"],
   transform: {
     "^.+\\.jsx?$": "babel-jest",
   },
