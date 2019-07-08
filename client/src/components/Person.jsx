819d7e83a771cbc6ca42176ece51aaa8fed8e4bd
Will Resen <willresen@gmail.com>
2019-07-07 20:38:19 -0700

    Implement "set" function to retrieve the movies for selected cast/crew

--- client/src/components/Person.jsx
+++ client/src/components/Person.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Person = ({info}) => (
+const Person = ({info, set}) => (
   <div className="container">
-    <div className="headshot"><img src={info._id.thumbnail_url}></img></div>
+    <div className="headshot" onClick={() => set(info._id._id)}><img src={info._id.thumbnail_url}></img></div>
     <div className="name">{info._id.name}</div>
     <div className="role">{info.role}</div>
   </div>
