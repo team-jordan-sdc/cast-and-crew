e31202b58958cc264758c8f094f09bb10b1b1cd1
Will Resen <willresen@gmail.com>
2019-07-09 13:24:52 -0700

    Add classes for rounded ratings, infoplate text and stars

--- client/src/components/Movie.jsx
+++ client/src/components/Movie.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const Movie = ({movie}) => (
+const Movie = ({ movie }) => (
   <div className="personnelMovie">
     <div className="movie_info">
       <div className="title">{movie.title}</div>
-      <div className="text">{movie.release_date}</div>
+      <div className="container">
+        <div className="infoplate_text">{movie.release_date}</div>
+        <div className="infoplate_text rounded">{movie.rating}</div>
+        <div className="infoplate_text rounded">{movie.runtime}</div>
+      </div>
+      <div className="container">
+        <div className="star_rating"><img src="https://mapquiz.app/fec/infoplate/Star_empty.svg"></img></div>
+      </div>
       <div className="text">{`Rent/Buy from ${movie.price}`}</div>
     </div>
     <div className="movie_thumbnail"><img src={movie.thumbnail_url}></img></div>
