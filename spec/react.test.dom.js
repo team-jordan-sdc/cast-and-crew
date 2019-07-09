e52dbc26651b83ec3457ae9e3bd2f025e5854774
Will Resen <willresen@gmail.com>
2019-07-08 19:16:41 -0700

    Separate mocked data into its own file

--- spec/react.test.dom.js
+++ spec/react.test.dom.js
@@ -1,44 +1,10 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import App from '../client/src/App.jsx';
+import mockData from './mockData.js';
 
 describe('App.jsx component', () => {
-
-//This should be in its own file, but moving the
-//object prevents the json property from being
-//called as a function...  I don't know why.
-  const promise = Promise.resolve({
-    "json": function () { return this }, //mock fetch's response.json() function
-    "_id": "5d218036d379e56725e8686b",
-    "title": "The Matrix",
-    "release_date": "May 12",
-    "vudu_rating": 3,
-    "rt_rating": 82,
-    "price": "$9.99",
-    "thumbnail_url": "https://mapquiz.app/fec/thumbnails/movie_thumbnail32.jpeg",
-    "personnel": [
-      {
-        "_id": {
-          "_id": "1",
-          "name": "Will",
-          "thumbnail_url": "https://mapquiz.app/fec/headshots/headshot16.jpeg",
-        },
-        "role": "Yodelist",
-      },
-      {
-        "_id": {
-          "_id": "2",
-          "name": "Resen",
-          "thumbnail_url": "https://mapquiz.app/fec/headshots/headshot16.jpeg",
-        },
-        "role": "Lead Guitarist",
-      },
-    ],
-  });
-
-  global.fetch = jest.fn(() => promise);
   let wrapper = mount(<App/>);
-
   /* If the App mounts successfully, remount before each test */
   beforeEach(() => {
     wrapper.mount();
@@ -66,6 +32,9 @@ describe('App.jsx component', () => {
     expect(wrapper.find('MovieCarousel').html()).toBe(null);
   })
 
+  test('Clicking a Person component should call fetch()', () => {
+    console.log(fetch.mock.calls);
+  })
 
 
 
