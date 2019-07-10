e0845fee166ee5d20d8fded4345afd757a29cb71
Will Resen <willresen@gmail.com>
2019-07-09 17:10:50 -0700

    Remove unnecessary async/await and setTimeouts

--- spec/react.test.dom.js
+++ spec/react.test.dom.js
@@ -32,23 +32,17 @@ describe('App.jsx component', () => {
     expect(wrapper.find('MovieCarousel').html()).toBe(null);
   })
 
-  test('Clicking a Person component should call fetch()', async (done) => {
-    await wrapper.find('div.headshot').first().simulate('click');
-    setTimeout(() => {
-      wrapper.update();
-      expect(fetch.mock.calls.length).toBe(3);
-      done();
-    }, 0)
+  test('Clicking a Person component should call fetch()', (done) => {
+    wrapper.find('div.headshot').first().simulate('click');
+    expect(fetch.mock.calls.length).toBe(3);
+    done();
   })
 
-  test('Clicking a Person component should render movies in the MovieCarousel', async (done) => {
-    await wrapper.find('div.headshot').first().simulate('click');
-    setTimeout(() => {
-      wrapper.update();
-      expect(wrapper.state().featuredPersonnel[0].title).toBe('The Matrix');
-      expect(wrapper.find('MovieCarousel').html()).toContain('The Matrix');
-      done();
-    }, 0)
+  test('Clicking a Person component should render movies in the MovieCarousel', (done) => {
+   wrapper.find('div.headshot').first().simulate('click');
+   expect(wrapper.state().featuredPersonnel[0].title).toBe('The Matrix');
+   expect(wrapper.find('MovieCarousel').html()).toContain('The Matrix');
+   done();
   })
 
 })
