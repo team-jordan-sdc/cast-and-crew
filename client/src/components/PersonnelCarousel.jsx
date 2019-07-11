e96722a9cb42e37bf4308b18d344a8db3c415b79
Will Resen <willresen@gmail.com>
2019-07-10 21:08:40 -0700

    Replace JQuery with ReactDOM in moveForward(); begin implementing styled components

--- client/src/components/PersonnelCarousel.jsx
+++ client/src/components/PersonnelCarousel.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Person from './Person.jsx';
 import $ from 'jquery';
+import { CarouselContainer, Title, Container, NavBackward, NavForward } from '../styling.jsx';
 
 class PersonnelCarousel extends React.Component {
   constructor(props){
@@ -25,8 +27,10 @@ class PersonnelCarousel extends React.Component {
   /* Restrict the carousel from moving too far forward. Its left position
      should not extend further than width of carousel - width of window. */
   moveForward(){
-    if(($('#personnel_carousel').position().left - 800) * -1 > $('#personnel_carousel').width() - $(window).width()) {
-      this.setState({position: ($('#personnel_carousel').width() - $(window).width()) * -1});
+    const position = ReactDOM.findDOMNode(this.refs['PersonnelCarousel']).getBoundingClientRect();
+
+    if((position.left - 800) * -1 > position.width - window.innerWidth) {
+      this.setState({position: (position.width - window.innerWidth) * -1});
     } else {
       this.setState({position: this.state.position - 800});
     }
@@ -34,16 +38,16 @@ class PersonnelCarousel extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <span className="main_title">Cast & Crew</span>
-        <div className="carousel_container">
-          <div id="p_backward" onClick={this.moveBackward}></div>
-          <div id="personnel_carousel" style={{ transform: `translate3d(${this.state.position}px, 0px, 0px)` }}>
+      <Container>
+        <Title>Cast & Crew</Title>
+        <CarouselContainer>
+          <NavBackward onClick={this.moveBackward} />
+          <div ref="PersonnelCarousel" id="personnel_carousel" style={{ transform: `translate3d(${this.state.position}px, 0px, 0px)` }}>
             {this.props.personnel.map(person => <div className="person"><Person info={person} set={this.props.set} /></div>)}
           </div>
-          <div id="p_forward" onClick={this.moveForward}></div>
-        </div>
-      </div>
+          <NavForward onClick={this.moveForward} />
+          </CarouselContainer>
+      </Container>
     )
   }
 
