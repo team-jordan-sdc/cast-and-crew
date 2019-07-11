42705440cdc3585a0207110450bd5ba1c53242b1
Will Resen <willresen@gmail.com>
2019-07-10 17:53:14 -0700

    Remove babel-transform-runtime plugin

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,7 @@
 module.exports = function (api) {
   const isTest = api.env('test');
   const presets = ['@babel/preset-env', '@babel/preset-react'];
-  const plugins = ['@babel/plugin-transform-runtime'];
   return {
     presets,
-    plugins
   };
 };
